4422cfc0819198b555c5d0d8ba458b61ab5d2487
SlimWoody <125416780+SlimWoody@users.noreply.github.com>
2023-06-22 21:00:27 +0400

    Add HomeWork - Phone book

--- Lesson07/Task01.py
+++ Lesson07/Task01.py
@@ -1,8 +1,8 @@
 from os import path
 
 file_base = "base.txt"
-last_id = 0
 all_data = []
+last_id = 0
 
 if not path.exists(file_base):
     with open(file_base, "w", encoding="utf-8") as _:
@@ -10,21 +10,22 @@ if not path.exists(file_base):
 
 
 def read_records():
-    global last_id, all_data
+    global all_data, last_id
 
-    with open(file_base, encoding="utf-8") as f:
+    with open(file_base, "r", encoding="utf-8") as f:
         all_data = [i.strip() for i in f]
         if all_data:
-            last_id = int(all_data[-1].split()[0])
-            return all_data
-    return []
+            last_id = int(all_data[-1][0])
+
+        return all_data
 
 
 def show_all():
-    if all_data:
-        print(*all_data, sep="\n")
-    else:
+    if not all_data:
         print("Empty data")
+    else:
+        print(*all_data, sep="\n")
+
 
 
 def add_new_contact():
@@ -39,31 +40,134 @@ def add_new_contact():
         f.write(f"{last_id} {string}\n")
 
 
+def del_contact():
+    global all_data
+
+    symbol = "\n"
+    show_all()
+    del_record = input("Enter the record id: ")
+
+    if exist_contact(del_record, ""):
+        all_data = [k for k in all_data if k[0] != del_record]
+
+        with open(file_base, 'w', encoding="utf-8") as f:
+            f.write(f'{symbol.join(all_data)}\n')
+        print("Record deleted!\n")
+    else:
+        print("The data is not correct!")
+
+
+def change_contact(data_tuple):
+    global all_data
+    symbol = "\n"
+
+    record_id, num_data, data = data_tuple
+
+    for i, v in enumerate(all_data):
+        if v[0] == record_id:
+            v = v.split()
+            v[int(num_data)] = data
+            if exist_contact(0, " ".join(v[1:])):
+                print("The data already exists!")
+                return
+            all_data[i] = " ".join(v)
+            break
+
+    with open(file_base, 'w', encoding="utf-8") as f:
+        f.write(f'{symbol.join(all_data)}\n')
+    print("Record changed!\n")
+
+
+def search_contact():
+    search_data = exist_contact(0, input("Enter the search data: "))
+    if search_data:
+        print(*search_data, sep="\n")
+    else:
+        print("The data is not correct!")
+
+
+def exist_contact(rec_id, data):
+    if rec_id:
+        candidates = [i for i in all_data if rec_id in i[0]]
+    else:
+        candidates = [i for i in all_data if data in i]
+    return candidates
+
+
+def data_collection(num):
+    answer = input(f"Enter a {num}: ")
+    while True:
+        if num in "surname name patronymic":
+            if answer.isalpha():
+                break
+        if num == "phone number":
+            if answer.isdigit() and len(answer) == 11:
+                break
+        answer = input(f"Data is incorrect!\n"
+                    f"Use only use only the letters"
+                    f" of the alphabet, the length"
+                    f" of the number is 11 digits\n"
+                    f"Enter a {num}: ")
+    return answer
+
+
 def main_menu():
     play = True
     while play:
         read_records()
         answer = input("Phone book:\n"
-        "1. Show all records\n"
-        "2. Add a record\n"
-        "3. Search a record\n"
-        "4. Change\n"
-        "5. Delete\n"
-        "6. Exp/Imp\n"
-        "7. Exit\n")
+                    "1. Show all records\n"
+                    "2. Add a record\n"
+                    "3. Search\n"
+                    "4. Change\n"
+                    "5. Delete\n"
+                    "6. Exp/Imp\n"
+                    "7. Exit\n")
         match answer:
             case "1":
                 show_all()
             case "2":
                 add_new_contact()
             case "3":
-                pass
+                search_contact()
             case "4":
-                pass
+                work = edit_menu()
+                if work:
+                    change_contact(work)
             case "5":
+                del_contact()
+            case "6":
+                pass
+            case "7":
                 play = False
             case _:
                 print("Try again!\n")
+
+
+def edit_menu():
+    add_dict = {"1": "surname", "2": "name", "3": "patronymic", "4": "phone number"}
+
+    show_all()
+    record_id = input("Enter the record id: ")
+
+    if exist_contact(record_id, ""):
+        while True:
+            print("\nChanging:")
+            change = input("1. surname\n"
+                        "2. name\n"
+                        "3. patronymic\n"
+                        "4. phone number\n"
+                        "5. exit\n")
+
+            match change:
+                case "1" | "2" | "3" | "4":
+                    return record_id, change, data_collection(add_dict[change])
+                case "5":
+                    return 0
+                case _:
+                    print("The data is not recognized, repeat the input.")
+    else:
+        print("The data is not correct!")
 
 
 main_menu()
